ee74eea9c24373f615b8e1907e812ae64d70f9e1
EdonaElshani <edonaelshani31@gmail.com>
2023-03-30 11:29:41 +0200

    vite modify

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/3D-Portfolio/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    assetsInlineLimit: 100000000,
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
+  },
+});
